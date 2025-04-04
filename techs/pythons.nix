9072c7359013e5c68b115bc98ad06968c7d669b3
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2025-04-04 17:09:48 +0200

    Rewrite python version drvs, and how default is setup (& switch to 3.12)

--- techs/pythons.nix
+++ techs/pythons.nix
@@ -1,41 +1,60 @@
 { pkgs, lib, pkgsChannels, mybuilders, ... }:
 
 let
-  inherit (pkgsChannels) stable bleedingedge;
-
-  ipythonForPy = pyDrv: binNameSuffix: (
-    let
-      ipyEnv = pyDrv.withPackages (pypkgs: [ pypkgs.ipython pypkgs.rich ]);
-    in mybuilders.linkBin "ipython${binNameSuffix}" "${ipyEnv}/bin/ipython"
+  pyVersion = (
+    {
+      pyDrv,
+      poetryDrv,
+      binNameSuffix,
+    }:
+    pkgs.buildEnv {
+      name = "python-tools-bins-${binNameSuffix}";
+      paths = [
+        (
+          let
+            ipyEnv = pyDrv.withPackages (pypkgs: [ pypkgs.ipython pypkgs.rich ]);
+          in mybuilders.linkBin "ipython${binNameSuffix}" "${ipyEnv}/bin/ipython"
+        )
+        (mybuilders.linkBin "python${binNameSuffix}" pyDrv)
+        (mybuilders.linkBin "poetry${binNameSuffix}" (poetryDrv.override { python3 = pyDrv; }))
+      ];
+      passthru.usedBinNameSuffix = binNameSuffix;
+      passthru.usedPythonDrv = pyDrv;
+    }
   );
 
-  pkgsForPy = pyDrv: poetryDrv: binNameSuffix: [
-    (ipythonForPy pyDrv binNameSuffix)
-    (mybuilders.linkBin "python${binNameSuffix}" pyDrv)
-    (mybuilders.linkBin "poetry${binNameSuffix}" (poetryDrv.override { python3 = pyDrv; }))
-  ];
-
-  recentPythons = pkgs.buildEnv {
-    name = "recent-pythons";
-    paths = lib.flatten [
-      (pkgsForPy stable.python312 stable.poetry "3.12")
-      # wants to rebuild stuff... it's the default anyway, let's just avoid it here
-      # (pkgsForPy bleedingedge.python313 bleedingedge.poetry "3.13")
+  pyDefault = pyVersionDrv: pkgs.buildEnv {
+    name = "default-python";
+    paths = [
+      pyVersionDrv.usedPythonDrv # MAYBE: remove useless binaries?
+      (mybuilders.linkBin "ipython" "${pyVersionDrv}/bin/ipython${pyVersionDrv.usedBinNameSuffix}")
+      # I want Poetry v2+ 😬
+      # (mybuilders.linkBin "poetry" "${pyVersionDrv}/bin/poetry${pyVersionDrv.usedBinNameSuffix}")
     ];
   };
+in
+let
 
-  defaultPython = pkgs.buildEnv {
-    name = "default-python";
-    paths = let defaultPython = bleedingedge.python313; in [
-      defaultPython
-      (ipythonForPy defaultPython "")
-      bleedingedge.poetry
-    ];
+  inherit (pkgsChannels) stable bleedingedge;
+
+  pyVersionDrvs = {
+    "3.12" = pyVersion {
+      pyDrv = stable.python312;
+      poetryDrv = stable.poetry;
+      binNameSuffix = "3.12";
+    };
+    "3.13" = pyVersion {
+      pyDrv = bleedingedge.python313;
+      poetryDrv = bleedingedge.poetry;
+      binNameSuffix = "3.13";
+    };
   };
 
 in {
   environment.systemPackages = [
-    recentPythons
-    defaultPython
+    pyVersionDrvs."3.12"
+    pyVersionDrvs."3.13"
+    (pyDefault pyVersionDrvs."3.12")
+    bleedingedge.poetry # I want last version poetry (v2+)
   ];
 }
